ff6757560e55fd4ae44066e7317287de8b6330d6
JackCT1 <jackthomas933@gmail.com>
2023-06-15 21:41:28 +0100

    imported and used user router

--- express-project/server.js
+++ express-project/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const userRouter = require("./routes/users");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -6,5 +7,7 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index", { text: "Text to render" });
 });
+
+app.use("/users", userRouter);
 
 app.listen(3000);
