f010e6820935b3a067f16ce378995edae053fd85
JackCT1 <jackthomas933@gmail.com>
2023-06-15 22:10:20 +0100

    dynamic id get request

--- express-project/routes/users.js
+++ express-project/routes/users.js
@@ -9,4 +9,12 @@ router.get("/new", (req, res) => {
   res.send("users new form");
 });
 
+router.post("/", (req, res) => {
+  res.send("Create user");
+});
+
+router.get("/:id", (req, res) => {
+  res.send(`Get user with ID ${req.params.id}`);
+});
+
 module.exports = router;
