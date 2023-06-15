ba210c60df3289526949d15ce40247588f2bc874
JackCT1 <jackthomas933@gmail.com>
2023-06-15 22:51:55 +0100

    .route method to combine

--- express-project/routes/users.js
+++ express-project/routes/users.js
@@ -13,16 +13,16 @@ router.post("/", (req, res) => {
   res.send("Create user");
 });
 
-router.get("/:id", (req, res) => {
-  res.send(`Get user with ID ${req.params.id}`);
-});
-
-router.put("/:id", (req, res) => {
-  res.send(`Update user with ID ${req.params.id}`);
-});
-
-router.delete("/:id", (req, res) => {
-  res.send(`Remove user with ID ${req.params.id}`);
-});
+router
+  .route("/:id")
+  .get((req, res) => {
+    res.send(`Get user with ID ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`Update user with ID ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`Remove user with ID ${req.params.id}`);
+  });
 
 module.exports = router;
