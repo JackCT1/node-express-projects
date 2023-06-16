ec7c4ab7b32d0663f5a5967c2302055d953e7163
JackCT1 <jackthomas933@gmail.com>
2023-06-16 15:08:41 +0100

    render static page with middleware

--- express-project/server.js
+++ express-project/server.js
@@ -2,12 +2,19 @@ const express = require("express");
 const userRouter = require("./routes/users");
 const app = express();
 
+app.use(express.static("public"));
+
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
+app.get("/", logger, (req, res) => {
   res.render("index", { text: "Text to render" });
 });
 
 app.use("/users", userRouter);
+
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
 
 app.listen(3000);
