22870413811ef3576a3455569c38a9d99391afea
JackCT1 <jackthomas933@gmail.com>
2023-06-18 23:58:01 +0100

    renamed subscriber file

--- beginner-api/routes/subscribers.js
+++ beginner-api/routes/subscribers.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Subscriber = require("../models/subcriber");
+const Subscriber = require("../models/subscriber");
 
 router.get("/", async (req, res) => {
   try {
