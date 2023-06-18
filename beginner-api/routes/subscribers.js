b767f819af7981934491ec2798a9b5a46744b295
JackCT1 <jackthomas933@gmail.com>
2023-06-18 23:57:10 +0100

    fixed model error

--- beginner-api/routes/subscribers.js
+++ beginner-api/routes/subscribers.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const Subscriber = require("../models/subcriber");
+
+router.get("/", async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find();
+    res.json(subscribers);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 router.get("/", (req, res) => {
   res.send("Hello World!");
