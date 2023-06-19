3ed8a470389d28c777669a8b3879b7dd22893d6a
JackCT1 <jackthomas933@gmail.com>
2023-06-19 17:43:38 +0100

    renamed subscribedtochannel

--- beginner-api/routes/subscribers.js
+++ beginner-api/routes/subscribers.js
@@ -19,7 +19,18 @@ router.get("/:id", (req, res) => {
   res.send(req.params.id);
 });
 
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscribedToChannel: req.body.subscribedToChannel,
+  });
+  try {
+    const newSubscriber = await subscriber.save();
+    res.status(201).json(newSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 router.patch("/:id", (req, res) => {});
 
