ea86f00e16dfb887959ee7e5a3af9c8bf8cc5949
JackCT1 <jackthomas933@gmail.com>
2023-06-21 21:49:24 +0100

    router setup

--- user-api/index.js
+++ user-api/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+import usersRoutes from "./routes/users";
+
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
+
+app.use("/users", usersRoutes);
 
 app.get("/", (req, res) => res.send("Hello world"));
 
--- user-api/routes/users.js
+++ user-api/routes/users.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 // get all users
-router.get("/users", (req, res) => {
+router.get("/", (req, res) => {
   res.send("Hello");
 });
 
