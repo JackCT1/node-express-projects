dd4dc0bea22d1ed73a5ad37f409cf0aa108f0a54
JackCT1 <jackthomas933@gmail.com>
2023-06-21 23:48:30 +0100

    get request for users

--- user-api/routes/users.js
+++ user-api/routes/users.js
@@ -2,9 +2,22 @@ const express = require("express");
 
 const router = express.Router();
 
+const users = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+  },
+  {
+    firstName: "Jane",
+    lastName: "Smith",
+    age: 30,
+  },
+];
+
 // get all users
 router.get("/", (req, res) => {
-  res.send("Hello");
+  res.send(users);
 });
 
 module.exports = router;
