3a98e9045d9459a1662f1251e24fb9759aab1206
JackCT1 <jackthomas933@gmail.com>
2023-06-23 17:44:24 +0100

    update user route completed

--- user-api/routes/users.js
+++ user-api/routes/users.js
@@ -37,10 +37,23 @@ router.get("/:id", (req, res) => {
   res.send(foundUser);
 });
 
+// delete a user
 router.delete("/:id", (req, res) => {
   const { id } = req.params.id;
   const filteredUsers = users.filter((user) => user.id != id);
   res.send(`user with id ${id} deleted.`);
+});
+
+// update a user
+router.patch("/:id", (req, res) => {
+  const { id } = req.params.id;
+  const { firstName, lastName, age } = req.body;
+
+  const updatedUser = users.find((user) => user.id == id);
+
+  if (firstName) updatedUser.firstName = firstName;
+  if (lastName) updatedUser.lastName = lastName;
+  if (age) updatedUser.age = age;
 });
 
 module.exports = router;
