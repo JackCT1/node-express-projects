5cf0907163fb5cbab2b49e673f0cd5cec740bc33
JackCT1 <jackthomas933@gmail.com>
2023-06-24 15:48:35 +0100

    all functions copied

--- user-api/controllers/users.js
+++ user-api/controllers/users.js
@@ -1,4 +1,6 @@
-export const getAllUsers = (req, res) => {};
+export const getAllUsers = (req, res) => {
+  res.send(users);
+};
 
 export const createUser = (req, res) => {
   const user = req.body;
@@ -8,8 +10,23 @@ export const createUser = (req, res) => {
   console.log(`User with username ${firstName}${lastName} added.`);
 };
 
-export const getUser = (req, res) => {};
+export const getUser = (req, res) => {
+  const { id } = req.params.id;
+  const foundUser = users.find((user) => user.id == id);
+  res.send(foundUser);
+};
 
-export const updateUser = (req, res) => {};
+export const updateUser = (req, res) => {
+  const { id } = req.params.id;
+  const { firstName, lastName, age } = req.body;
+
+  const updatedUser = users.find((user) => user.id == id);
+
+  if (firstName) updatedUser.firstName = firstName;
+  if (lastName) updatedUser.lastName = lastName;
+  if (age) updatedUser.age = age;
+
+  res.send(`User with id ${id} has been updated.`);
+};
 
 export const deleteUser = (req, res) => {};
