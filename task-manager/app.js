c19c5e168e1ab15e9c6d25d647dabc64f6c75dd0
JackCT1 <jackthomas933@gmail.com>
2023-06-26 23:19:16 +0100

    basic get request

--- task-manager/app.js
+++ task-manager/app.js
@@ -3,4 +3,9 @@ const app = express();
 
 const PORT = 3000;
 
+//routes
+app.get("/", (req, res) => {
+  res.send("Task Manager App");
+});
+
 app.listen(PORT, console.log(`Server is listening on port ${PORT}`));
