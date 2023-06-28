def735b169397ba48cdd70699e6eef6f2e5be060
JackCT1 <jackthomas933@gmail.com>
2023-06-28 21:46:42 +0100

    importing mongoose

--- task-manager/db/connect.js
+++ task-manager/db/connect.js
@@ -1,3 +1,5 @@
 require("dotenv").config();
 
+const mongoose = require("mongoose");
+
 const connectionString = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}${process.env.ATLAS_CLUSTER}s.sbfv6yu.mongodb.net/?retryWrites=true&w=majority`;
