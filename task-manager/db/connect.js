194f79d26239a6d976f2f91a643c8cd51a2495a3
JackCT1 <jackthomas933@gmail.com>
2023-06-28 22:10:52 +0100

    successfully connected to database

--- task-manager/db/connect.js
+++ task-manager/db/connect.js
@@ -2,4 +2,9 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 
-const connectionString = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}${process.env.ATLAS_CLUSTER}s.sbfv6yu.mongodb.net/?retryWrites=true&w=majority`;
+const connectionString = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@${process.env.ATLAS_CLUSTER}s.sbfv6yu.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(connectionString)
+  .then(() => console.log("Connected to Database"))
+  .catch((err) => console.log(err));
