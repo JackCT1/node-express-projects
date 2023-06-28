955d196ee1e520e9005dbd9791304047be1d1450
JackCT1 <jackthomas933@gmail.com>
2023-06-28 21:31:04 +0100

    connection to atlas

--- task-manager/db/connect.js
+++ task-manager/db/connect.js
@@ -1,1 +1,3 @@
-const connectionString = "";
+require("dotenv").config();
+
+const connectionString = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}${process.env.ATLAS_CLUSTER}s.sbfv6yu.mongodb.net/?retryWrites=true&w=majority`;
